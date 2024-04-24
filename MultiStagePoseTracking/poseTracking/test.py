53d91e1aab60418e7639ecb0b79c3ce13aa6c07e
benburnham <burnham1@bu.edu>
2024-04-24 17:08:51 -0400

    mmpose results

--- MultiStagePoseTracking/poseTracking/test.py
+++ MultiStagePoseTracking/poseTracking/test.py
@@ -1,16 +1,28 @@
-# Check Pytorch installation
-import torch, torchvision
+# # Check Pytorch installation
+# import torch, torchvision
 
-print('torch version:', torch.__version__, torch.cuda.is_available())
-print('torchvision version:', torchvision.__version__)
+# print('torch version:', torch.__version__, torch.cuda.is_available())
+# print('torchvision version:', torchvision.__version__)
 
-# Check MMPose installation
-import mmpose
+# # Check MMPose installation
+# import mmpose
 
-print('mmpose version:', mmpose.__version__)
+# print('mmpose version:', mmpose.__version__)
 
-# Check mmcv installation
-from mmcv.ops import get_compiling_cuda_version, get_compiler_version
+# # Check mmcv installation
+# from mmcv.ops import get_compiling_cuda_version, get_compiler_version
 
-print('cuda version:', get_compiling_cuda_version())
-print('compiler information:', get_compiler_version())
+# print('cuda version:', get_compiling_cuda_version())
+# print('compiler information:', get_compiler_version())
+
+from mmpose.apis import inference_topdown, init_model
+from mmpose.utils import register_all_modules
+
+register_all_modules()
+
+config_file = 'td-hm_hrnet-w48_8xb32-210e_coco-256x192.py'
+checkpoint_file = 'td-hm_hrnet-w48_8xb32-210e_coco-256x192-0e67c616_20220913.pth'
+model = init_model(config_file, checkpoint_file, device='cpu')  # or device='cuda:0'
+
+# please prepare an image with person
+results = inference_topdown(model, 'demo.jpg')
