07d9804a98dff1e0c296d01c60d2f8c6c92bea44
benburnham <burnham1@bu.edu>
2024-04-24 18:22:42 -0400

    pose output

--- MultiStagePoseTracking/poseTracking/test.py
+++ MultiStagePoseTracking/poseTracking/test.py
@@ -1,28 +1,53 @@
-# # Check Pytorch installation
-# import torch, torchvision
+# Check Pytorch installation
+import torch, torchvision
 
-# print('torch version:', torch.__version__, torch.cuda.is_available())
-# print('torchvision version:', torchvision.__version__)
+print('torch version:', torch.__version__, torch.cuda.is_available())
+print('torchvision version:', torchvision.__version__)
 
-# # Check MMPose installation
-# import mmpose
+# Check MMPose installation
+import mmpose
 
-# print('mmpose version:', mmpose.__version__)
+print('mmpose version:', mmpose.__version__)
 
-# # Check mmcv installation
-# from mmcv.ops import get_compiling_cuda_version, get_compiler_version
+# Check mmcv installation
+from mmcv.ops import get_compiling_cuda_version, get_compiler_version
 
-# print('cuda version:', get_compiling_cuda_version())
-# print('compiler information:', get_compiler_version())
+print('cuda version:', get_compiling_cuda_version())
+print('compiler information:', get_compiler_version())
 
-from mmpose.apis import inference_topdown, init_model
-from mmpose.utils import register_all_modules
+# from mmpose.apis import inference_topdown, init_model
+# from mmpose.utils import register_all_modules
 
-register_all_modules()
+# register_all_modules()
 
-config_file = 'td-hm_hrnet-w48_8xb32-210e_coco-256x192.py'
-checkpoint_file = 'td-hm_hrnet-w48_8xb32-210e_coco-256x192-0e67c616_20220913.pth'
-model = init_model(config_file, checkpoint_file, device='cpu')  # or device='cuda:0'
+# config_file = 'mmpose/configs/body_2d_keypoint/rtmo/coco/rtmo-l_16xb16-600e_coco-640x640.py'
+# checkpoint_file = 'poseTracking/rtmo-l_16xb16-600e_coco-640x640-516a421f_20231211.pth'
+# model = init_model(config_file, checkpoint_file, device='cpu')  # or device='cuda:0'
 
-# please prepare an image with person
-results = inference_topdown(model, 'demo.jpg')
+# # please prepare an image with person
+# results = inference_topdown(model, 'poseTracking/example.jpg', vis_out_dir='poseTracking/vis_results.jpg')
+# print(results)
+
+
+# https://github.com/open-mmlab/mmpose/blob/main/docs/en/user_guides/inference.md
+from mmpose.apis import MMPoseInferencer
+# img_path = 'poseTracking/example.jpg'
+img_path = '../../FINAL DATASET/ALL/train/Uchi Mata/Uchi Mata_train_10.mp4'
+
+
+# build the inferencer with model alias
+inferencer = MMPoseInferencer('rtmo')
+
+# build the inferencer with model config name
+# inferencer = MMPoseInferencer('rtmo-l_16xb16-600e_coco-640x640')
+
+# # build the inferencer with model config path and checkpoint path/URL
+# inferencer = MMPoseInferencer(
+#     pose2d='mmpose/configs/body_2d_keypoint/rtmo/coco/rtmo-l_16xb16-600e_coco-640x640.py',
+#     pose2d_weights='poseTracking/rtmo-l_16xb16-600e_coco-640x640-516a421f_20231211.pth'
+# )
+
+result_generator = inferencer(img_path, out_dir='poseTracking')
+# result = next(result_generator)
+results = [result for result in result_generator]
+
